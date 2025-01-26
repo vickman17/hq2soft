78faf04eb938210c151af676b0fa2e37a5722dba
Vickman17 <victorymadumere6@gmail.com>
2025-01-26 13:25:02 +0100

    0.0.6

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -2,19 +2,14 @@ package io.ionic.starter;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.plugins.notification.LocalNotifications;
-import com.pusher.pushnotifications.PushNotifications;
+import com.getcapacitor.community.localnotifications.LocalNotificationsPlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Initialize Pusher Push Notifications
-        PushNotifications.start(getApplicationContext(), "fef21ad9-18fb-4c2a-9bac-64eb6f197664");
-        PushNotifications.addDeviceInterest("hello");
-
-        // Initialize Local Notifications
-        registerPlugin(LocalNotifications.class);
+        // Register Local Notifications Plugin
+        registerPlugin(LocalNotificationsPlugin.class);
     }
 }
